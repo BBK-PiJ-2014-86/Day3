2b3122e77073ba523388c8e55c98331f7fb314fe
BBK-PiJ-2014-86 <andreangeorgiev@yahoo.com>
2014-10-17 17:16:14 +0100

    test1
    test1

---
+++ Test1111.java
@@ -0,0 +1,34 @@
+package wowow;
+
+public class Test1111 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//random
+		//random//random
+		//random
+		//random
+		//random
+		//random
+		//random
+		//random
+		//random//random
+		//random
+		//random
+		//random
+		//random
+		//random
+		//random
+		//randomv
+		//random
+		//random
+		//random
+		
+		
+	}
+
+}
